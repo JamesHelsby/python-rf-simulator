751879e11910d3a12a9d535e433bfdf63b6837d7
Jamie <jamiehelsby@gmail.com>
2024-08-28 17:42:47 +0100

    updated data collection

--- data_collection_full_random.py
+++ data_collection_full_random.py
@@ -1,5 +1,4 @@
 from ship import Ship
-# from ship_analyzer import plot_container_network
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -10,12 +9,12 @@ LAYERS = 25
 CONTAINER_TYPE = "standard"
 ITERATIONS = 2
 TYPE = "plane"
+CONTROLLER = "number" # "distance" or "number"
 
-# Define the ranges
 jammer_power_ranges = np.arange(18, 24.1, 0.5)
 distance_ranges = np.arange(0, 146.1, 0.1)
+num_nodes_ranges = range(1, 21)
 
-# Shuffle the ranges to make the iterations random
 np.random.shuffle(jammer_power_ranges)
 np.random.shuffle(distance_ranges)
 
@@ -28,19 +27,34 @@ def save_to_csv(results, power):
     df = pd.DataFrame([results])
     df = df[['power', 'distance', 'status'] + [col for col in df.columns if col not in ['power', 'distance', 'status']]]
 
-    csv_filename = f"{BAYS}x{ROWS}x{LAYERS}-{CONTAINER_TYPE}-{TYPE}.csv"
+    csv_filename = f"{BAYS}x{ROWS}x{LAYERS}-{CONTAINER_TYPE}-{TYPE}-{CONTROLLER}.csv"
     df.to_csv(csv_filename, mode='a', index=False, header=not pd.io.common.file_exists(csv_filename))
 
 for power in tqdm(jammer_power_ranges, desc="Jammer Power"):
-    for distance in tqdm(distance_ranges, desc="Distance", leave=False):
-        ship = Ship(bays=BAYS, rows=ROWS, layers=LAYERS)
-        ship.add_containers(":", ":", ":", container_type=CONTAINER_TYPE)
+    if CONTROLLER == "number":
+        for num_nodes in tqdm(num_nodes_ranges, desc="Number of Nodes", leave=False):
+            ship = Ship(bays=BAYS, rows=ROWS, layers=LAYERS)
+            ship.add_containers(":", ":", ":", container_type=CONTAINER_TYPE)
+            
+            if TYPE == "plane":
+                ship.set_n_nodes_in_plane('bays', int(BAYS / 2), num_nodes, malicious=True, jammer=True, transmit_power=power)
+            else:
+                ship.set_n_nodes(num_nodes, malicious=True, jammer=True, transmit_power=power)
+            
+            ship.generate_container_graph()
+            results = ship.analyse_graph()
+            save_to_csv(results, power)
 
-        if TYPE == "plane":
-            ship.set_max_nodes_in_plane('bays', int(BAYS / 2), distance, malicious=True, jammer=True, transmit_power=power)
-        else:
-            ship.set_max_nodes(distance, malicious=True, jammer=True, transmit_power=power)
+    else:  # CONTROLLER == "distance"        
+        for distance in tqdm(distance_ranges, desc="Distance", leave=False):
+            ship = Ship(bays=BAYS, rows=ROWS, layers=LAYERS)
+            ship.add_containers(":", ":", ":", container_type=CONTAINER_TYPE)
 
-        ship.generate_container_graph()
-        results = ship.analyse_graph()
-        save_to_csv(results, power)
+            if TYPE == "plane":
+                ship.set_max_nodes_in_plane('bays', int(BAYS / 2), distance, malicious=True, jammer=True, transmit_power=power)
+            else:
+                ship.set_max_nodes(distance, malicious=True, jammer=True, transmit_power=power)
+
+            ship.generate_container_graph()
+            results = ship.analyse_graph()
+            save_to_csv(results, power)
